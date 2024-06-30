9b4919e17974473a5389791a11c69d063533201c
shashi0104 <s.sekhar1092i@gmail.com>
2024-06-30 15:52:57 +0530

    Post mapping with request body

--- initial/src/main/java/com/example/restservice/EmployeeController.java
+++ initial/src/main/java/com/example/restservice/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.example.restservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +12,10 @@ public class EmployeeController {
     @GetMapping("/employees")
     public Employees getAllEmployees() {
         return employeeManager.getEmployees();
+    }
+    
+    @PostMapping("/employees")
+    public void addEmployee(@RequestBody Employee newEmployee) {
+        employeeManager.getEmployees().getEmployeeList().add(newEmployee);
     }
 }
